1712a14991a8959c6f99ae4920c0a156e4dc418f
vikumkodikara <vikumkodikara123@gmail.com>
2026-05-08 00:19:03 +0530

    test(tempconv): add unit tests for Temperature Converter application
    Add Spring Boot test class for TempconvApplication to verify application context loads correctly.

---
+++ tempconv/src/test/java/com/nima/tempconv/TempconvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nima.tempconv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempconvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
