ef6c10b1e3269a6537ca517f9f5d02855f1e3e38
vikumkodikara <vikumkodikara123@gmail.com>
2026-07-01 01:04:45 +0530

    Update TemperatureRepository

--- tempconv/src/main/java/com/nima/tempconv/repository/TemperatureRepository.java
+++ tempconv/src/main/java/com/nima/tempconv/repository/TemperatureRepository.java
@@ -1,8 +1,13 @@
 package com.nima.tempconv.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.nima.tempconv.model.TemperatureLog;
 
 public interface TemperatureRepository extends MongoRepository<TemperatureLog, String> {
+
+    // Exercise 2: Spring Data derived query method to find logs by input unit
+    List<TemperatureLog> findByInputUnit(String inputUnit);
 }
